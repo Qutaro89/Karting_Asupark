a88f65f73eeb8ac83d7eb241000aa482f3bc8eb0
Qutaro89 <157788291+Qutaro89@users.noreply.github.com>
2026-04-13 09:44:35 +0200

    subida del script sql de la base de datos

---
+++ base_de_datos/asupark.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-04-2026 a las 12:52:13
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `asupark`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiempos`
+--
+
+CREATE TABLE `tiempos` (
+  `ID_USUARIO` int(11) NOT NULL,
+  `TIEMPO_VUELTA` time(3) NOT NULL,
+  `FECHA_HORA_VUELTA` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tiempos`
+--
+
+INSERT INTO `tiempos` (`ID_USUARIO`, `TIEMPO_VUELTA`, `FECHA_HORA_VUELTA`) VALUES
+(1, '00:01:07.740', '2026-04-12 12:10:40');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_USUARIO` int(11) NOT NULL,
+  `NOMBRE_USUARIO` varchar(50) NOT NULL,
+  `CORREO_USUARIO` varchar(50) NOT NULL,
+  `FECHA_ALTA` date NOT NULL,
+  `CONTRA_USUARIO` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_USUARIO`, `NOMBRE_USUARIO`, `CORREO_USUARIO`, `FECHA_ALTA`, `CONTRA_USUARIO`) VALUES
+(1, 'admInistrador', 'estecorreonoexiste@gmail.com', '2026-04-12', 'admin123');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tiempos`
+--
+ALTER TABLE `tiempos`
+  ADD PRIMARY KEY (`ID_USUARIO`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_USUARIO`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tiempos`
+--
+ALTER TABLE `tiempos`
+  ADD CONSTRAINT `tiempos_ibfk_1` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuarios` (`ID_USUARIO`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
